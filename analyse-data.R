7a087565b8d4739e72a3f9aa453ba6ed918f6d78
An Chu <chuvanan.cva@gmail.com>
2017-06-13 18:00:47 +0700

    visualise distribution of scores

--- analyse-data.R
+++ analyse-data.R
@@ -10,17 +10,9 @@ if (interactive()) {
 
 setwd("~/Documents/diem-thi-lop10-2017/")
 
-dt.chuyen <- readxl::read_xls("diem-ts10-2017baochi-1497260000353.xls", sheet = 1)
+dt.chuyen <- read.csv("diem-ts10-lopchuyen.csv", stringsAsFactors = FALSE)
 
-dt.thuong <- readxl::read_xls("diem-ts10-2017baochi-1497260000353.xls", sheet = 2)
-
-## sensible colnames
-
-names(dt.thuong) <- c("Sbd", "Ho", "Ten", "Van", "Nn", "Toan")
-
-dt.thuong <- subset(dt.thuong, Van >= 0 & Nn >= 0 & Toan >= 0)
-
-dt.thuong <- dt.thuong[, c("Van", "Nn", "Toan")]
+dt.thuong <- read.csv("diem-ts10-lopthuong.csv", stringsAsFactors = FALSE)
 
 ## -----------------------------------------------------------------------------
 ## explore data
@@ -28,15 +20,15 @@ dt.thuong <- dt.thuong[, c("Van", "Nn", "Toan")]
 
 ## distribution of scores for each subject
 
-dt.toan <- table(dt.thuong$Toan, dnn = "diem")
+dt.toan <- table(dt.thuong$toan, dnn = "diem")
 dt.toan <- as.data.frame(dt.toan, responseName = "n_toan", stringsAsFactors = FALSE)
 dt.toan$diem <- as.double(dt.toan$diem)
 
-dt.van <- table(dt.thuong$Van, dnn = "diem")
+dt.van <- table(dt.thuong$van, dnn = "diem")
 dt.van <- as.data.frame(dt.van, responseName = "n_van", stringsAsFactors = FALSE)
 dt.van$diem <- as.double(dt.van$diem)
 
-dt.nn <- table(dt.thuong$Nn, dnn = "diem")
+dt.nn <- table(dt.thuong$ngoaingu, dnn = "diem")
 dt.nn <- as.data.frame(dt.nn, responseName = "n_nn", stringsAsFactors = FALSE)
 dt.nn$diem <- as.double(dt.nn$diem)
 
@@ -48,7 +40,6 @@ dt.toan <- merge(dt.toan,
                  by = "diem",
                  all = TRUE)
 dt.toan$n_toan[is.na(dt.toan$n_toan)] <- 0
-
 
 dt.van <- merge(dt.van,
                 data.frame(diem = seq(0, 10, 0.25)),
@@ -65,15 +56,44 @@ dt.nn$n_nn[is.na(dt.nn$n_nn)] <- 0
 ## compare distributions
 
 
-x.range <- c(0, 10)
 y.range <- range(dt.toan$n_toan, dt.van$n_van, dt.nn$n_nn)
+van.col <- adjustcolor("blue", alpha.f = 0.4)
+toan.col <- adjustcolor("red4", alpha.f = 0.4)
+nn.col <- adjustcolor("forestgreen", alpha.f = 0.4)
 
-plot.new()
-plot.window(x.range, y.range)
-barplot(dt.van$n_van, col = adjustcolor("blue", alpha.f = 0.3), axes = FALSE, border = FALSE)
-barplot(dt.nn$n_nn, col = adjustcolor("forestgreen", alpha.f = 0.3), add = TRUE, axes = FALSE, border = FALSE)
-barplot(dt.toan$n_toan, col = adjustcolor("red4", alpha.f = 0.3), axes = FALSE, border = FALSE, add = TRUE)
+png("pho-diem-thi.png", width = 464, height = 664, res = 97)
 
-## lines(dt.toan$diem, dt.toan$n_toan, type = "h", col = "red4")
-## lines(dt.van$diem, dt.van$n_van, type = "h", col = "blue")
-## lines(dt.nn$diem, dt.nn$n_nn, type = "h", col = "forestgreen")
+par(mfrow = c(3, 1),
+    mar = c(0, 0, 0, 0),
+    oma = c(3, 2, 4, 2),
+    bg = "gray98",
+    family = "Lato")
+
+barplot(dt.van$n_van, col = van.col,
+        axes = FALSE, border = FALSE,
+        ylim = y.range)
+axis(1, col = "gray98", col.ticks = "gray30", labels = FALSE)
+mtext("Văn", line = -3, adj = 0)
+
+barplot(dt.toan$n_toan, col = toan.col,
+        axes = FALSE, border = FALSE,
+        ylim = y.range)
+axis(1, col = "gray98", col.ticks = "gray30", labels = FALSE)
+mtext("Toán", line = -3, adj = 0)
+
+barplot(dt.nn$n_nn, col = nn.col,
+        axes = FALSE, border = FALSE,
+        ylim = y.range)
+axis(1, col = "gray98", col.ticks = "gray30", at = seq(0, 50, 10), labels = seq(0, 10, 2))
+mtext("Ngoại ngữ", line = -3, adj = 0)
+
+## tile
+mtext("Phổ điểm thi lớp 10 - khóa 2017", outer = TRUE, line = 2, font = 2)
+
+dev.off()
+
+## dev.size("px")
+## 464 664
+
+## dev.size("in")
+## 4.833333 6.898513
